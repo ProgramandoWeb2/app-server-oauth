7d2001a119dbe5724ea82d3eb03cad4c69340c2d
ProgramandoWeb2 <daplicacionesmoviles1@gmail.com>
2024-10-05 19:13:13 -0500

    Auth server

--- src/main/java/pe/edu/cibertec/app_server_oauth/config/AuthorizationServerConfig.java
+++ src/main/java/pe/edu/cibertec/app_server_oauth/config/AuthorizationServerConfig.java
@@ -1,5 +1,9 @@
 package pe.edu.cibertec.app_server_oauth.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,20 +13,41 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
 import java.util.UUID;
 
 @Configuration
 public class AuthorizationServerConfig {
+    private KeyPair keyPair;
+
+    public AuthorizationServerConfig(){
+        try{
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator
+                    .getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        }catch (Exception ex){
+            throw new RuntimeException("Error a generar claves RSA", ex);
+        }
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -60,6 +85,35 @@ public class AuthorizationServerConfig {
         return new InMemoryUserDetailsManager(user);
     }
 
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        return new NimbusJwtEncoder(jwkSource());
+    }
 
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(
+                (RSAPublicKey) keyPair.getPublic()).build();
+    }
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings(){
+        return AuthorizationServerSettings.builder()
+                .jwkSetEndpoint("/.well-known/jwks.json")
+                .build();
+    }
 
+    @Bean
+    public KeyPair jwkSetKeyPair(){
+        return keyPair;
+    }
+    //Crea el JWKSource a partir de una Key
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(){
+        RSAKey rsaKey = new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                .privateKey((RSAPrivateKey) keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return ((jwkSelector, securityContext) -> jwkSelector.select(jwkSet));
+    }
 }
